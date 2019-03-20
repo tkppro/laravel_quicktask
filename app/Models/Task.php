e731b79c9d663e96cebc690679db8e86580cf0bc
Thang Dang <tkpproo@gmail.com>
2019-03-20 09:44:58 +0700

    create task controller and modify route

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -17,6 +17,6 @@ class Task extends Model
 
     public function user()
     {
-        return $this->belongsTo(App::User);
+        return $this->belongsTo(User::class);
     }
 }
---
+++ resources/lang/en/common.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'task' => [
+        'create' => [
+            'success' => 'Task has been successfully created.',
+            'fail' => 'Cannot create Task.'
+        ],
+        'delete' => [
+            'success' => 'Task has been successfully deleted',
+            'fail' => 'Cannot delete Task.',
+        ]
+    ],
+
+];
